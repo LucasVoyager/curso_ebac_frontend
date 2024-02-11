6426108a8e215f7025c1385d0d7f56264d52b24a
Lucas barros <fallenagamesytb@gmail.com>
2024-02-11 17:56:47 -0300

    build: construindo automação de compressão de arquivos via grunt

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compres: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            }
+        }
+    })
+
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+}
