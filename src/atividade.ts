2d23360b790f6751dd928c2533ec33d2a93ad861
Lucas barros <fallenagamesytb@gmail.com>
2024-03-03 23:14:07 -0300

    Build: criando atividade com typescript

---
+++ src/atividade.ts
@@ -0,0 +1,13 @@
+const calculaMultiplicacao = (a1: number, a2:number) => a1 * a2;
+
+const falaOi = (nome: string) => `Olá ${nome}`;
+
+
+const number1 = 98;
+const number2 = 56;
+
+console.log(calculaMultiplicacao(number1, number2));
+
+const nome = 'Lucas';
+
+console.log(falaOi(nome));
